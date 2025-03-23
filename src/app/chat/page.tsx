f1ace38f28e49c0d06d4863d112f32d6e444179b
Franklin Marcelo Reyes Banegas <freyeshn96@outlook.es>
2025-03-23 10:26:03 -0600

    fix: solve problem with middleware.

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,19 +1,9 @@
 import React from "react";
-import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
-import { cookies } from "next/headers";
-import { redirect } from "next/navigation";
 import ChatSideBar from "./ChatSideBar";
 import DocumentSideBar from "./DocumentSideBar";
 
 const page = async () => {
-  const supabase = createServerComponentClient({ cookies });
-  const {
-    data: { session },
-  } = await supabase.auth.getSession();
 
-  if (!session) {
-    redirect("/login");
-  }
   return (
     <div
       className="w-full h-full flex flex-row"
