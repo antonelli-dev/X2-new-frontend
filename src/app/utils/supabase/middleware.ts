acd89920d6931f0315431aac45d1bc8b965224b6
Franklin Marcelo Reyes Banegas <freyeshn96@outlook.es>
2025-03-26 09:53:58 -0600

    improve middleware

--- src/app/utils/supabase/middleware.ts
+++ src/app/utils/supabase/middleware.ts
@@ -29,18 +29,23 @@ export async function updateSession(request: NextRequest) {
 
   const {
     data: { user },
-  } = await supabase.auth.getUser()
+  } = await supabase.auth.getUser();
 
-  if (
-    !user &&
-    !request.nextUrl.pathname.startsWith('/login') &&
-    !request.nextUrl.pathname.startsWith('/auth')
-  ) {
-    // no user, potentially respond by redirecting the user to the login page
-    const url = request.nextUrl.clone()
-    url.pathname = '/login'
-    return NextResponse.redirect(url)
+  const { pathname } = request.nextUrl;
+  const publicPaths = ['/login', '/register'];
+
+  if (user && publicPaths.includes(pathname)) {
+    const url = request.nextUrl.clone();
+    url.pathname = '/chat';
+
+    return NextResponse.redirect(url);
   }
-  
+
+  if (!user && !publicPaths.includes(pathname)) {
+    const url = request.nextUrl.clone();
+    url.pathname = '/login';
+    return NextResponse.redirect(url);
+  }
+
   return supabaseResponse
 }
